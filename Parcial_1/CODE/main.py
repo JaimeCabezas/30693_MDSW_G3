e3b9b689b8aa26bd564b43707482db37dad8ad88
Alexis <n03060023@gmail.com>
2026-06-02 00:07:25 -0500

    Fix: Crear carpeta uploads dinámicamente

--- Parcial_1/CODE/main.py
+++ Parcial_1/CODE/main.py
@@ -54,6 +54,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+os.makedirs('uploads', exist_ok=True)
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 intentos_login = {}
